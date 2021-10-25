0b76889ca3ac901023eec56dc163a9b3ca9f863f
Astarosh <Astarosh@avansoma.no>
2021-10-25 14:24:00 +0300

    remove generated

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,10 +2,4 @@ export type { IUploadedDocument } from './interfaces/api/advoca/IUploadedDocumen
 export type { IFeedBack, ISaveToDataBase } from './interfaces/api/advoca/ISaveToDataBase';
 export * from './interfaces/jwtClaims';
 export * from './interfaces/enums';
-export * from './interfaces/enums/shared/enums';
-export * from './interfaces/enums/portal/enums';
-export * from './interfaces/enums/advoca/enums';
-export * as schemaDirectory from './generated/directory/schema';
-export * as schemaProxy from './generated/proxy/schema';
-export * as schemaChat from './generated/chat/schema';
 //# sourceMappingURL=index.d.ts.map
